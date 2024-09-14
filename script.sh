1fd5045aee934c43ff5e21da0be3911355efd55e
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-14 18:03:36 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,6 +13,8 @@ docker tag ecf8-183-87-250-107.ngrok-free.app/tomcat:alpine tomcat:alpine
 
 if [[ $VERSION =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT ]]
 then
+    echo "SNAPSHOT REPO SELECTED"
+
     echo "Build Image"
     docker build -t b16a-183-87-250-107.ngrok-free.app/demoapp:$VERSION .
 
@@ -26,9 +28,11 @@ then
     docker rmi b16a-183-87-250-107.ngrok-free.app/demoapp:$VERSION
 
 else
+    echo "RELEASE REPO SELECTED"
+
     echo "Build Image"
     docker build -t 21d6-183-87-250-107.ngrok-free.app/demoapp:$VERSION .
-    
+
     echo "Login To Docker Registry"
     echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin 21d6-183-87-250-107.ngrok-free.app
 
