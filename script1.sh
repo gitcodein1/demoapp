98da406b07a347d4a0c6a1895d9f0d559c62f9a8
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-15 13:01:49 +0530

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,67 @@
+#!bin/bash
+VERSION=$(mvn -q \
+        -Dexec.executable=echo \
+        -Dexec.args='${project.version}' \
+        --non-recursive \
+        exec:exec)
+
+proxy_repo="9211-183-87-250-107.ngrok-free.app"
+snapshot_repo="afad-183-87-250-107.ngrok-free.app"
+release_repo="f900-183-87-250-107.ngrok-free.app"
+
+
+
+echo "**Pull Base Image From Proxy Repo**"
+docker pull $proxy_repo/tomcat:alpine
+echo
+echo "**Tag the Pulled Image**"
+docker tag $proxy_repo/tomcat:alpine tomcat:alpine
+echo
+
+if [[ $VERSION =~ ^[0-9]+.[0-9]+.[0-9]+-SNAPSHOT ]]
+then
+    echo "--------------------------"
+    echo "* SNAPSHOT REPO SELECTED *"
+    echo "--------------------------"
+    echo ">> Build Image <<"
+    echo "-----------------"
+    docker build -t $snapshot_repo/demoapp:$VERSION .
+    echo
+    echo ">>Login To Docker Registry..."
+    echo "-----------------------------"
+    docker login -u $user -p $pass $snapshot_repo
+    echo
+    echo ">> Push Docker Image <<"
+    echo "-----------------------"
+    docker push $snapshot_repo/demoapp:$VERSION
+    echo
+    echo ">> Remove Image <<"
+    echo "------------------"
+    docker rmi $snapshot_repo/demoapp:$VERSION
+
+else
+    echo "-------------------------"
+    echo "* RELEASE REPO SELECTED *"
+    echo "-------------------------"
+    echo ">> Build Image <<"
+    echo "-----------------"
+    docker build -t $release_repo/demoapp:$VERSION .
+    echo
+    echo ">> Login To Docker Registry <<"
+    echo "-------------------------------"
+    docker login -u $user -p $pass $release_repo_repo
+    echo
+    echo ">> Push Docker Image <<"
+    echo "-----------------------"
+    docker push $release_repo/demoapp:$VERSION
+    echo
+    echo ">> Remove Image <<"
+    echo "------------------"
+    docker rmi $release_repo/demoapp:$VERSION
+fi
+
+echo
+echo ">> Clean UP <<"
+echo "--------------"
+docker rmi $proxy_repo/tomcat:alpine tomcat:alpine
+echo
